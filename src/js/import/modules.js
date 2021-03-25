645fc2d8979fe331b7a8ac3a641156be1de167fe
Dmitry <dmitriyinfo@gmail.com>
2021-03-25 13:11:28 +0300

    ss

---
+++ src/js/import/modules.js
@@ -0,0 +1,7 @@
+
+import "%modules%/plugins/lightbox.min";
+import "%modules%/plugins/owl.carousel.min";
+// import "%modules%/plugins/resizeSensor";
+import "%modules%/plugins/sticky.min";
+import "%modules%/header/header";
+import "%modules%/footer/footer"
